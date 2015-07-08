cd5b55cc7bac9ad3fa92227481a7ee5a8a342ebb
mobeets <mobeets@gmail.com>
2015-07-08 12:27:15 -0500

    reads requirements, fixes #1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Matlab Package Manager
 
-A simple Matlab package manager for downloading packages from Matlab Central's File Exchange, GitHub repositories, or any other url pointing to a .zip file.
+A simple Matlab package manager (inspired by [pip](https://github.com/pypa/pip)) for downloading packages from Matlab Central's File Exchange, GitHub repositories, or any other url pointing to a .zip file.
 
 ## Usage
 
@@ -10,15 +10,21 @@ __Setup__
 
 __Install a single file__
 
-`mpm -n export_fig -u https://codeload.github.com/altmany/export_fig/legacy.zip/master`
+`mpm -n export_fig -e https://codeload.github.com/altmany/export_fig/legacy.zip/master`
 
-`mpm -n mASD -u https://github.com/mobeets/mASD.git`
+`mpm -n mASD -e https://github.com/mobeets/mASD.git`
+
+__Install from list of requirements in file__
+
+`mpm -r requirements.txt`
+
+The requirements file should look something like this:
+
+    export-fig -e http://www.mathworks.com/matlabcentral/fileexchange/23629-export-fig?download=true
+    gridfitdir -e http://www.mathworks.com/matlabcentral/fileexchange/downloads/9937/akamai/gridfitdir.zip
+    mASD -e https://github.com/mobeets/mASD.git
 
 ## Coming soon
-
-__Install from `requirements.txt`__
-
-`mpm -f requirements.txt`
 
 __Install to temporary folder__
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os.path
 import argparse
+import urllib2
+import shutil
 from StringIO import StringIO
 from zipfile import ZipFile
-from urllib import urlopen
-import shutil
 
 HOMEDIR = os.path.expanduser("~")
 MATLABDIR = os.path.join(HOMEDIR, 'Documents', 'MATLAB')
@@ -17,9 +17,31 @@ def copytree(src, dst, symlinks=False, ignore=None):
         else:
             shutil.copy2(s, d)
 
+def readzip(url):
+    try:
+        urlc = urllib2.urlopen(url)
+        return ZipFile(StringIO(urlc.read()))
+    except Exception, e:
+        # print url, e
+        return
+    # except urllib2.HTTPError, e:
+    #     print e.code
+    #     return
+    # except urllib2.URLError, e:
+    #     print e.args
+    #     return
+
 def unzip(url, outdir):
-    urlc = urlopen(url)
-    zipfile = ZipFile(StringIO(urlc.read()))
+    zipfile = readzip(url)
+    if zipfile is None and '.git' in url:
+        i = url.rfind('.git')
+        url0 = url[:i] + '/zipball/master' + url[i+4:]
+        zipfile = readzip(url0)
+    if zipfile is None and 'fileexchange' in url:
+        zipfile = readzip(url + '?download=true')
+    if zipfile is None:
+        return False
+
     dirnames = set([os.path.normpath(x).split(os.sep)[0] for x in zipfile.namelist()])
     zipfile.extractall(outdir)
     if len(dirnames) == 1:
@@ -28,21 +50,40 @@ def unzip(url, outdir):
         # for f in os.listdir(basedir):
         #   shutil.copy2(os.path.join(basedir, f), outdir)
         shutil.rmtree(basedir)
+    return True
     
 def main(url, name, outdir, force):
+    url = url.strip()
+    name = name.strip()
     if not os.path.exists(outdir):
         raise Exception("Invalid MATLABDIR: {0}".format(outdir))
     outdir = os.path.join(outdir, name)
     if os.path.exists(outdir) and not force:
-        print 'Package already exists at {0}'.format(outdir)
+        print 'Package "{1}" already exists at {0}'.format(outdir, name)
         return
-    unzip(url, outdir)
+    status = unzip(url, outdir)
+    if status:
+        print 'Installed "{1}" to {0}'.format(outdir, name)
+    else:
+        print 'ERROR: Could not install "{0}"'.format(name)
+
+def load_from_file(infile, outdir, force):
+    with open(infile) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if '-e' not in line: continue
+            name, _, url = line.partition('-e')
+            main(url, name, outdir, force)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--name", type=str, required=True, default=None)
-    parser.add_argument("-u", "--url", type=str, required=True, default=None)
-    parser.add_argument("-o", "--outdir", type=str, default=MATLABDIR)
-    parser.add_argument("-f", "--force", action='store_true', default=False)
+    parser.add_argument("-n", "--name", type=str, required=False, default=None, help="name of package")
+    parser.add_argument("-r", "--reqsfile", type=str, required=False, default=None, help="path to requirements file")
+    parser.add_argument("-e", "--url", type=str, required=False, default=None, help="url of package as zip")
+    parser.add_argument("-o", "--outdir", type=str, default=MATLABDIR, help="installation directory")
+    parser.add_argument("-f", "--force", action='store_true', default=False, help="overwrite if package already exists")
     args = parser.parse_args()
-    main(args.url, args.name, args.outdir, args.force)
+    if args.reqsfile:
+        load_from_file(args.reqsfile, args.outdir, args.force)
+    else:
+        main(args.url, args.name, args.outdir, args.force)
