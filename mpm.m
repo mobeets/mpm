e9b80edfb28ad911b00efa8ee236abd3edc98b3a
mobeets <mobeets@gmail.com>
2020-06-07 09:17:37 -0400

    handling api.github urls, fixes #67

--- mpm.m
+++ mpm.m
@@ -321,7 +321,8 @@ end
 function url = handleCustomUrl(url, release_tag)
     
     % if .git url, must remove and add /zipball/master
-    inds = strfind(url, '.git');
+    inds = strfind(url, '.git'); % want to match this
+    inds = setdiff(inds, strfind(url, '.github')); % ignore matches to '.github'
     if isempty(inds)
         inds = strfind(url, '?download=true');
         if isempty(inds)
