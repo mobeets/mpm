cccc8c89993d74cce70a1f4cc14d8f1b43ef2190
Dennis Hoer <dennis.hoer@tusimple.ai>
2020-06-08 01:16:27 -0700

    download zip for .git extension first then fallback on git clone

--- test/test_install.m
+++ test/test_install.m
@@ -1,3 +1,6 @@
+% cd test/
+% result = runtests('test_install')
+% table(result)
 clear
 warning('off','backtrace')
 
@@ -5,80 +8,101 @@ mpm_dir = fullfile(pwd, '..');
 addpath(mpm_dir)
 
 
-%
-% Test Install - using GitHub api (no url)
-%
 
-%% Test install of latest release
+% Test API Install - using GitHub api (no url)
+
+%% Test install api latest
 mpm install export_fig --force
 export_fig_dir = fullfile(mpm_dir, 'mpm-packages', 'export_fig');
 assert(exist(fullfile(export_fig_dir, 'export_fig.m'), 'file')==2)
 assert(~isempty(which('export_fig')))
 
-%% Test install of specific tag
+%% Test install api tag
 mpm install matlab2tikz -t 0.4.7 --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'version-0.4.7'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
-%% Test install of specific branch
+%% Test install api branch
 mpm install matlab2tikz -t develop --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'test/suites/ACID.Octave.4.2.0.md5'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
-%% Test install of specific commit hash
+%% Test install api commit hash
 mpm install matlab2tikz -t ca56d9f --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'version-0.3.3'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
 
-%
-% Test Install - using URL with .git file extension
-%
 
-%% Test install of default branch (master)
+% Test URL Install - using URL with .git file extension
+
+%% Test install url default branch
 mpm install matlab2tikz -u https://github.com/matlab2tikz/matlab2tikz.git --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'src/matlab2tikz.m'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
-%% Test install of specific tag
+%% Test install url tag
 mpm install matlab2tikz -t 0.4.7 -u https://github.com/matlab2tikz/matlab2tikz.git --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'version-0.4.7'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
-%% Test install of specific branch
+%% Test install url branch
 mpm install matlab2tikz -t develop -u https://github.com/matlab2tikz/matlab2tikz.git --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'test/suites/ACID.Octave.4.2.0.md5'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
-%% Test install of specific commit hash
+%% Test install url commit hash
 mpm install matlab2tikz -t ca56d9f -u https://github.com/matlab2tikz/matlab2tikz.git --force
 matlab2tikz_dir = fullfile(mpm_dir, 'mpm-packages', 'matlab2tikz');
 assert(exist(fullfile(matlab2tikz_dir, 'version-0.3.3'), 'file')==2)
 assert(~isempty(which('matlab2tikz')))
 
 
-%
-% Test Install - using MathWorks FileExchange
-%
 
-%% Test download directly from fileexchange
+% Test Git Clone Install - using non-GitHub URL with .git file extension
+
+%% Test install git clone default branch
+mpm install hello -u https://bitbucket.org/dhoer/mpm_test.git --force
+mpm_test_dir = fullfile(mpm_dir, 'mpm-packages', 'hello');
+assert(exist(fullfile(mpm_test_dir, 'hello.m'), 'file')==2)
+assert(~isempty(which('hello')))
+
+%% Test install git clone tag
+mpm install hello -t v1.0.0 -u https://bitbucket.org/dhoer/mpm_test.git --force
+mpm_test_dir = fullfile(mpm_dir, 'mpm-packages', 'hello');
+assert(exist(fullfile(mpm_test_dir, 'v1.0.0'), 'file')==2)
+assert(~isempty(which('hello')))
+
+%% Test install git clone branch
+mpm install hello -t develop -u https://bitbucket.org/dhoer/mpm_test.git --force
+mpm_test_dir = fullfile(mpm_dir, 'mpm-packages', 'hello');
+assert(exist(fullfile(mpm_test_dir, 'v2.0.0'), 'file')==2)
+assert(~isempty(which('hello')))
+
+% not working - bitbucket clone of branch using hash not supported
+%%% Test install git clone commit hash
+%mpm install hello -t 36967c34800121b957a2855b8fcf4491dd13866c -u https://bitbucket.org/dhoer/mpm_test.git --force
+%mpm_test_dir = fullfile(mpm_dir, 'mpm-packages', 'hello');
+%assert(exist(fullfile(mpm_test_dir, 'v1.1.0'), 'file')==2)
+%assert(~isempty(which('hello')))
+
+
+
+%% Test install FileExchange
 mpm install covidx -u https://www.mathworks.com/matlabcentral/fileexchange/76213-covidx --force
 covidx_dir = fullfile(mpm_dir, 'mpm-packages', 'covidx');
 assert(exist(fullfile(covidx_dir, 'covidx.m'), 'file')==2)
 assert(~isempty(which('covidx')))
 
 
-%
-% Test Uninstall
-%
 
-%% Test that everything is removed
+%% Test uninstall
 mpm install colorbrewer --force
 mpm uninstall colorbrewer --force
 colorbrewer_dir = fullfile(mpm_dir, 'mpm-packages', 'colorbrewer');
@@ -87,21 +111,24 @@ assert(exist(fullfile(colorbrewer_dir, 'brewermap.m'), 'file')==0)
 assert(isempty(which('brewermap')))
 
 
-%
-% Test Freeze
-%
 
-%% Test freeze returns 3 installed packages
+%% Test freeze
 results = evalc('mpm freeze');
 assert(contains(results,'export_fig'))
 assert(contains(results,'matlab2tikz==ca56d9f'))
 assert(contains(results,'covidx'))
 
 
-%
-% Test Search
-%
 
-%% Test search returns proper
+%% Test search
 results = evalc('mpm search export_fig');
 assert(contains(results,'Found url: https://api.github.com/repos/altmany/export_fig/zipball/v3.'))
+
+
+
+%% Test infile
+cd(mpm_dir)
+mpm install --approve --force -i requirements-example.txt
+assert(~isempty(which('export_fig')))
+assert(~isempty(which('matlab2tikz')))
+assert(~isempty(which('brewermap')))
